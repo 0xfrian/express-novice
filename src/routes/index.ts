32e8ef058ffee028015737172440e365e7a0f13a
brian-fong <0xfrian@gmail.com>
2024-06-22 21:03:40 -0700

    🔁 refactor: code cleanup

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,5 @@ import { Request } from "express";
 import { Response } from "express";
 
 export default function helloWorld(_req: Request, res: Response) {
-  // Returns "hello world!"
   res.status(200).send("hello world!");
 }
